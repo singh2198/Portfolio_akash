47874791aab0dcc41a4ab0518d0cf0c9a2ff843b
singh2198 <8755akash@gmail.com>
2022-12-24 19:30:39 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import About from './Component/About';
-import Contact from './Component/Contact';
-import Experience from './Component/Experience';
-import Home from './Component/Home';
+// import About from './Component/About';
+// import Contact from './Component/Contact';
+// import Experience from './Component/Experience';
+// import Home from './Component/Home';
 // import { BrowserRouter } from 'react-router-dom';
 
 import Nav from './Component/Nav';
-import Project from './Component/Project';
-import SocialLinks from './Component/SocialLinks';
+// import Project from './Component/Project';
+// import SocialLinks from './Component/SocialLinks';
 
 
 
